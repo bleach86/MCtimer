d8c5b744a8171627f6b9be2563255f0a5190ffc1
bleach86 <josh.leach86@yahoo.com>
2020-06-14 20:07:38 -0500

    add license

--- mctimer.py
+++ mctimer.py
@@ -6,6 +6,9 @@ import datetime
 import time
 import tkinter as tk
 
+#"THE BEER-WARE LICENSE" (Revision 42):
+#bleach86 wrote this file. As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return
+
 system_type = platform.system()
 if system_type == 'Linux':
     directory = os.path.expanduser('~/.minecraft/saves/')
