408fee7d0788c57531617b2ee1acb26bf8de0b3e
bleach86 <josh.leach86@yahoo.com>
2020-08-10 23:47:58 -0500

    update for v2
    update to add global hotkeys, borderless window(even visible over full screen), handle versions 1.7-1.16+ automatically, and configuration file.

--- mctimer.py
+++ mctimer.py
@@ -1,36 +1,48 @@
 import os
+import sys
 import platform
 import json
 import glob
 import datetime
 import time
-from nbt.nbt import NBTFile
 import tkinter as tk
+from bindglobal import BindGlobal
 
 #"THE BEER-WARE LICENSE" (Revision 42):
 #bleach86 wrote this file. As long as you retain this notice you can do whatever you want with this stuff. 
 #If we meet some day, and you think this stuff is worth it, you can buy me a beer in return
 
+json_file = 'mct_config.json'
+with open(json_file) as json_file:
+    data2 = json.load(json_file)
+    
+
 system_type = platform.system()
 if system_type == 'Linux':
-    directory = os.path.expanduser('~/.minecraft/saves/')
+    directory = os.path.expanduser(data2['linux_saves'])
 elif system_type == 'Darwin':
-    directory = os.path.expanduser('~/Library/Application Support/minecraft/saves/')
+    directory = os.path.expanduser(data2['mac_saves'])
 elif system_type == 'Windows':
-    directory = os.path.expanduser('~\\AppData\\Roaming\\.minecraft\\saves\\')
+    directory = os.path.expanduser(data2['windows_saves'])
 
 last_amount = 0
 window = tk.Tk()
+bg = BindGlobal(widget=window)
 window.text = tk.StringVar()
 window.text2 = tk.StringVar()
 rt = time.time()
-click1 = 1
+if data2['auto_start'] == 'true':
+    click1 = 1
+    click2 = 1
+else:
+    click1 = 0
+    click2 = 0
 
 def get_time():
 
     try:
         global last_amount
-        time.sleep(0.1)
+        #time.sleep(0.1)
         latest = max([os.path.join(directory,d) for d in os.listdir(directory)], key=os.path.getmtime)
         
         if system_type == "Linux" or system_type == "Darwin":
@@ -43,13 +55,8 @@ def get_time():
         
         with open(timer) as json_file:
             data = json.load(json_file)
-            level = NBTFile(latest + "/level.dat")
             try:
-                level = str(level["Data"]["Version"]["Name"])
-                if level >= '1.13':
-                    amount = data['stats']['minecraft:custom']['minecraft:play_one_minute']
-                else:
-                    amount = data['stat.playOneMinute']
+                amount = data['stats']['minecraft:custom']['minecraft:play_one_minute']
             except:
                 amount = data['stat.playOneMinute']
             json_file.close()
@@ -66,22 +73,46 @@ def get_time():
         return '0:00:00.000'
 
 def window2():
-    greeting = tk.Label(fg="lime", bg="black", font="Arial 45 bold", textvariable=window.text)
+    greeting = tk.Label(fg=data2['igt_color'], bg=data2['bg_color'], font="Arial 45 bold", textvariable=window.text)
     greeting.pack()
-    greeting2 = tk.Label(fg="red", bg="black", font="Arial 45 bold", textvariable=window.text2)
+    greeting2 = tk.Label(fg=data2['rta_color'], bg=data2['bg_color'], font="Arial 45 bold", textvariable=window.text2)
     greeting2.pack()
+    bg.gbind(data2['pause'], on_press)
+    bg.gbind(data2['reset_start'], on_press2)
+    bg.gbind(data2['exit'], clicked3)
     window.bind("<Button-1>", clicked)
+    window.bind("<Button-3>", clicked2)
     greeting.after(0, update_time)
     window.title("MCtimer")
     window.attributes('-topmost', True)
-    window.geometry("+0+0")
+    window.overrideredirect(True)
+    window.geometry(data2['window_pos'])
     window.mainloop()
 
 def update_time():
+    global rt
     window.text.set(get_time())
-    window.text2.set(real_time())
-    window.after(800, update_time)
-    
+    if click1 == 1:
+        window.text2.set(real_time())
+    elif click1 == 0:
+        rt = time.time()
+    if click2 == 0:
+        rt = time.time()
+        window.text2.set("0:00:00.000")
+    window.after(data2['rta_update'], update_time)
+
+def on_press(event):
+    left_click()
+
+def on_press2(event):
+    right_click()
+
+def clicked3(event):
+    sys.exit(1)
+
+def clicked2(event):
+    right_click()
+
 def clicked(event):
     left_click()
 
@@ -90,24 +121,39 @@ def left_click():
     if click1 == 1:
         click1 = 0
     elif click1 == 0:
+        click1 = 0
+
+def right_click():
+    global click1
+    global click2
+    if click2 == 1:
+        click1 = 0
+        click2 = 0
+    elif click2 == 0:
+        click2 = 1
         click1 = 1
 
 def real_time():
     global rt
     global click1
-    if get_time() == '0:00:00.000':
-        rt = time.time()
-        click1 = 1
-        return '0:00:00.000'
-    elif click1 == 1:
-        rt2 = time.time()
-        real_time = rt2 - rt
-        rtc = str(datetime.timedelta(seconds=real_time))
-        return rtc[:-3]
-        
-    elif click1 == 0:
-        rt = time.time()
-        return "0:00:00.000"
+    global click2
+    if data2['auto_start'] == 'true':
+        if get_time() == '0:00:00.000':
+            rt = time.time()
+            click1 = 1
+            click2 = 1
+            return '0:00:00.000'
+        elif click1 == 1:
+            rt2 = time.time()
+            real_time = rt2 - rt
+            rtc = str(datetime.timedelta(seconds=real_time))
+            return rtc[:-3]
+    else:
+        if click1 == 1:
+            rt2 = time.time()
+            real_time = rt2 - rt
+            rtc = str(datetime.timedelta(seconds=real_time))
+            return rtc[:-3]
 
 def main():
     window2()
